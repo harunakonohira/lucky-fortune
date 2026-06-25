a995639f86b24069a3a24c4a0e00dee00002ab5e
harunakonohira <harunakonohira613@gmail.com>
2026-06-25 19:50:49 +0900

    Viteプロジェクトを作成

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 class Fortune(BaseModel):
     result: str
     message: str
-
 
 
 @app.get("/fortune", response_model=Fortune)
